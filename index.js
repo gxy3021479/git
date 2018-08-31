abfafe5eb73467a63786837653d3361da1e8c583
gxy3021479 <269608864@qq.com>
2018-08-31 14:45:18 +0800

    q

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const path=require('path');
 const server=express();
 server.get('/',(req,res)=>{
-    res.send('ok'
+    res.send('马宏宇王八蛋'
         // path.resolve()
     );
 })
